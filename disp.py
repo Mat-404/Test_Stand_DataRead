aa288891b8914264021cfba6892273bed7586487
Mat-404 <cotem2021@my.fit.edu>
2023-02-28 10:26:19 -0500

    Update

--- disp.py
+++ disp.py
@@ -1,11 +1,19 @@
+import PySimpleGUI as sg
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.animation as animation
 import matplotlib.figure as mplfig
+from PIL import Image, ImageTk
 import tkinter as tk
-import serial
+from tkinter import *
+from serial import Serial
 import serial.tools.list_ports
+
+ports = serial.tools.list_ports.comports()
+
+portList = []
 
 def animate(i, line):
     y = np.random.randint(10, size=len(x))
@@ -25,7 +33,7 @@ class Calculator(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        self.wm_title('Beam calculator')
+        self.wm_title('Temperature and Pressure Data')
         container = tk.Frame(self)
         container.pack(side='top', fill='both', expand=True)
         for i, f in enumerate(figs):
@@ -39,8 +47,66 @@ class Calculator(tk.Tk):
 
 ser = serial.Serial()
 
+for onePort in ports:
+    try :
+        ser = serial.Serial(onePort, 115200, timeout=10)
+        ser.open()
+        portList.append(str(onePort) + " - Active")
+        ser.close()
+    except:
+        print("Serial port inactive.")
+
+if len(portList) == 0:
+    portList.append("No active ports found.")
+
+root = Tk()
+root.geometry("500x300")
+root.resizable(False, False)
+root.iconbitmap('Rocketry_Club_Logo.ico')
+root['bg'] = '#741012'
+root.title("Select a Port")
+
+my_listbox = Listbox(root)
+my_listbox['bg'] = '#af9878'
+my_listbox.grid(row=1, column=0, padx=10, pady=10)
+my_listbox.configure(background='#af9878', foreground='#741012', width=0)
+
+portVar = ""
+
+def select():
+    if my_listbox.curselection() == ():
+        my_label.config(text="Please Select a Port")
+    elif my_listbox.curselection() != "(No active ports found.)":
+        my_label.config(text=my_listbox.get(ANCHOR))
+        selectVar = my_listbox.get(ANCHOR)
+        a = portList.index(selectVar)
+        portVar = ports[a]
+        print(portVar)
+        root.destroy()
+    elif my_listbox.curselection() == "(No active ports found.)":
+        my_label.config(text="No active ports found.")
+
+my_text = Label(root, text="Select a Port to connect to:")
+my_text.grid(row=0, column=0, padx=10, pady=10)
+
+for i in portList:
+    my_listbox.insert(END, i)
+
+my_button = Button(root, text="Select Port", command=select)
+my_button.grid(row=2, column=0, padx=10, pady=10)
+
+photo = ImageTk.PhotoImage(file="Rocketry_Club_Logo.png")
+
+tk.Label(root, image=photo).grid(row=1, column=2, padx=60, pady=10)
+
+
+global my_label
+my_label = Label(root, text="")
+
+root.mainloop()
+
 try:
-    ser = serial.Serial('COM11', 115200, timeout=10)
+    ser = serial.Serial('portVar', 115200, timeout=10)
     ser.open()
 
     fig = plt.figure()
@@ -74,10 +140,12 @@ try:
                 axs[i].set_ylim([22, 28])
                 axs.ylabel('Temperature (C)')
                 axs.xlabel('Time (u)')
+                axs[i].set(facecolor='#741012')
             else:
                 axs[i].set_ylim([100, 120])
                 axs.ylabel('Pressure (kPa)')
                 axs.xlabel('Time (u)')
+                axs[i].set(facecolor='#741012')
 
         app = Calculator()
         app.configure(background='#af9878')
