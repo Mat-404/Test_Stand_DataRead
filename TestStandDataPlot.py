a6c9059473c1caace573c61bd7d0d75ee7027b2d
Mat-404 <cotem2021@my.fit.edu>
2023-03-20 16:11:13 -0400

    Restructure repo to become more readable

--- TestStandDataPlot.py
+++ TestStandDataPlot.py
@@ -15,30 +15,17 @@ serialInst = serial.Serial()
 
 portList = []
 
-dataMasterList = []
-
-for onePort in ports:
-    portList.append(str(onePort))
+for port in ports:
+    portList.append(str(port))
 
 print (portList)
 
+#Define functions
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
     return figure_canvas_agg
-
-root = Tk()
-root.geometry("500x300")
-root['bg'] = '#741012'
-root.title("Select a Port")
-
-my_listbox = Listbox(root)
-my_listbox['bg'] = '#af9878'
-my_listbox.grid(row=1, column=1, padx=10, pady=10)
-my_listbox.configure(background='#af9878', foreground='#741012', width=0)
-
-portVar = ""
 
 def select():
     if my_listbox.curselection() == ():
@@ -50,18 +37,30 @@ def select():
         portVar = ports[a]
         root.destroy()
 
-my_text = Label(root, text="Select a Port to connect to:")
-my_text.grid(row=0, column=1, padx=10, pady=10)
+root = Tk()
+root.geometry("500x300")
+root['bg'] = '#741012'
+root.title("Select a Port")
+
+#Window Element Code
+my_listbox = Listbox(root)
+my_listbox['bg'] = '#af9878'
+my_listbox.grid(row=1, column=1, padx=10, pady=10)
+my_listbox.configure(background='#af9878', foreground='#741012', width=0)
 
 for i in portList:
     my_listbox.insert(END, i)
 
+my_text = Label(root, text="Select a Port to connect to:")
+my_text.grid(row=0, column=1, padx=10, pady=10)
+
 my_button = Button(root, text="Select Port", command=select)
 my_button.grid(row=2, column=1, padx=10, pady=10)
 
+portVar = ""
+
 global my_label
 my_label = Label(root, text="")
-#my_label.pack(pady=5)
 
 root.mainloop()
 
@@ -73,7 +72,7 @@ serialInst.open()
 
 timeVals = []
 tempVals = []
-pressVals = []
+forceVals = []
 
 fig, (ax1, ax2) = plt.subplots(1,2)
 matplotlib.rcParams['text.color'] = '#F6C350'
@@ -96,10 +95,10 @@ ax1.plot(timeVals, tempVals, label = "Temperature vs Time")
 ax1.set_title("Temperature vs Time")
 ax1.set_ylim(0, 500)
 ax1.set(xlabel='Time (s)', ylabel='Temperature (C)')
-ax2.plot(timeVals, pressVals, label = "Pressure vs Time")
-ax2.set_title("Pressure vs Time")
+ax2.plot(timeVals, forceVals, label = "Load vs Time")
+ax2.set_title("Load vs Time")
 ax2.set_ylim(0, 200)
-ax2.set(xlabel='Time (s)', ylabel='Pressure (kPa)')
+ax2.set(xlabel='Time (s)', ylabel='Load (kg)')
 
 window2 = sg.Window("Engine Data Test", layout, finalize=True)
 window2.force_focus()
@@ -117,11 +116,10 @@ while(i < 500):
         packetList = packet.split(",")
         # ['24.21', '22.23', '102.30']
         if i > 10:
-            dataMasterList.append(packetList)
             print(packetList)
             print(timeVals)
             print(tempVals)
-            print(pressVals)
+            print(forceVals)
         i += 1
 
         # Take in data sequentially
@@ -129,7 +127,7 @@ while(i < 500):
             
             timeVals.append(float(packetList[0]))
             tempVals.append(float(packetList[1]))
-            pressVals.append(float(packetList[2]))
+            forceVals.append(float(packetList[2]))
 
         # Update the graph
         ax1.clear()
@@ -138,10 +136,10 @@ while(i < 500):
         ax1.set_title("Temperature vs Time")
         ax1.set_ylim(25, 55)
         ax1.set(xlabel='Time (s)', ylabel='Temperature (C)')
-        ax2.plot(timeVals, pressVals, label = "Pressure vs Time")
-        ax2.set_title("Pressure vs Time")
-        ax2.set_ylim(90, 120)
-        ax2.set(xlabel='Time (s)', ylabel='Pressure (kPa)')
+        ax2.plot(timeVals, forceVals, label = "Load vs Time")
+        ax2.set_title("Load vs Time")
+        ax2.set_ylim(0, 200)
+        ax2.set(xlabel='Time (s)', ylabel='Load (kg)')
         fig_canvas_agg.draw()
 
         if i%2 == 0:
@@ -157,8 +155,18 @@ if event == "-EXPORT2-":
     window3.force_focus()
     event2, values2 = window3.read()
     if event2 == "-CSV-":
-        print("Exporting as CSV...")
+        print("Exporting as CSV...")        
+        file = open("data.csv", "w")
+        file.truncate(0)
+        for i in range(timeVals):
+            file.write(str(timeVals[i]) + "," + str(tempVals[i]) + "," + str(forceVals[i]) + "\n")
+        file.close()
         window3.close()
     if event2 == "-TXT-":
         print("Exporting as TXT...")
+        file = open("data.txt", "w")
+        file.truncate(0)
+        for i in range(timeVals):
+            file.write(str(timeVals[i]) + "," + str(tempVals[i]) + "," + str(forceVals[i]) + "\n")
+        file.close()
         window3.close()
